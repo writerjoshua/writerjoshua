ab364cb3e954b3ce7150710e75354f9876c33239
Joshua Lucero <135270692+writerjoshua@users.noreply.github.com>
2026-02-16 15:49:23 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ function loadJSON(filename, type) {
     return;
   }
 
-  fetch(`./ assets/pages/${filename}.json`)
+  fetch(`./assets/pages/${filename}.json`)
     .then(response => {
       if (!response.ok) throw new Error('Data not found');
       return response.json();
